0e5e3e389dc4ed1c46ea77fbd0186bfbc06fe170
nihalmulla786 <nihalmullani05@gmail.com>
2025-04-13 11:17:48 +0530

    Add app/src/main/java/com/kolhapur/busbooking/MainActivity.kt 1744523267697

---
+++ app/src/main/java/com/kolhapur/busbooking/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.kolhapur.busbooking
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnSignup).setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
+    }
+}
